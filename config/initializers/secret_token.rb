746c0bf557cfccfca4d40db1557b8de9253f5622
gantzinis <gantzinis@gmail.com>
2012-08-22 20:58:04 +0300

    to proto mou commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstproject::Application.config.secret_token = 'c9194b6ae7b024a38b94c03a21329595311a6d3627836294d94d9178582c9d812928d7fbfe92cb9076a84c48ddb408b6d120ceebd0ce577ca58d99579e1e4943'
